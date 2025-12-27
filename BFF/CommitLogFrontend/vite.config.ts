fe4f44a191ebc366420222fc60a8d45fcc3b3245
Aisheel Nair <aisheelnair@outlook.com>
2025-12-27 09:43:42 +0200

    chore(frontend): add core frontend pnpm dependencies
    - TanStack Router for type-safe routing
    - TanStack Query + Immer & Zustand for state management
    - React Hook Form & Zod for schema-based form validation
    - Axios for API calls
    - Tailwind CSS for styling
    - React Hot Toast for global ui notifications

--- BFF/CommitLogFrontend/vite.config.ts
+++ BFF/CommitLogFrontend/vite.config.ts
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+  },
   plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    tailwindcss(),
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+});
